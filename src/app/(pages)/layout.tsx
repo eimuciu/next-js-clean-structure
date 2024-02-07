7ab25449108e177121cb292cc76710d068702f34
ed <allgoodata@outlook.com>
2024-02-07 11:58:57 +0200

    init commit

---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './style.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'cash-tracker',
+  description: 'Finance manager',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
